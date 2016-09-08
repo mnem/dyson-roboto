fa49fcbf42a0cdf6d1475843b4e2d324dcfd2b45
Todor Goranov <tpgoranov@gmail.com>
2016-09-08 17:04:32 +0100

    Adding imageFeed to the Commander

--- Ultratron/Ultratron/Commander.m
+++ Ultratron/Ultratron/Commander.m
@@ -16,11 +16,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Commander () <MQTTSessionDelegate>
 @property (nonatomic, nullable) MQTTSession *session;
+@property (nonatomic) NSURLSession *imageFeedSession;
+@property (nonatomic) NSTimer *imageFeedPollingTimer;
+@property (nonatomic) NSString *ipAddress;
 @end
 
 @implementation Commander
 
 - (void)connectToIPAddress:(NSString *)ipAddress handler:(ConnectionHandler)handler {
+    self.ipAddress = ipAddress;
     dispatch_block_t connector = ^{
         MQTTCFSocketTransport *transport = [[MQTTCFSocketTransport alloc] init];
         transport.host = ipAddress;
@@ -45,7 +49,9 @@ NS_ASSUME_NONNULL_BEGIN
     };
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0u), connector);
+    [self setUpImageFeed];
 }
+
 
 - (void)sendCommandDictionary:(NSDictionary *)command forTopic:(NSString *)topic {
     if (![NSJSONSerialization isValidJSONObject:command]) {
@@ -71,6 +77,52 @@ NS_ASSUME_NONNULL_BEGIN
 }
 
 #pragma mark - MQTTSessionDelegate
+
+#pragma mark - ImageFeedSetup
+
+- (void)setUpImageFeed{
+    [self setupURLSession];
+    [self subscribeForImageFeed];
+}
+
+- (void)setupURLSession {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    configuration.HTTPMaximumConnectionsPerHost = 1;
+    self.session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:[[NSOperationQueue alloc] init]];
+}
+
+- (void)subscribeForImageFeed
+{
+    _imageFeedPollingTimer = [NSTimer scheduledTimerWithTimeInterval:0.3
+                                                              target:self
+                                                            selector:@selector(poll)
+                                                            userInfo:nil
+                                                             repeats:YES];
+    NSLog(@"Subscribe for image Feed");
+    
+}
+
+#pragma mark - Poll
+
+- (void)poll
+{
+    NSLog(@"Poll for Image Feed");
+    NSURLSessionTask *task = [self.session dataTaskWithURL:[NSURL URLWithString:@"http://192.168.1.113:8080/frame.jpg"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        UIImage *image = [UIImage imageWithData:data];
+        NSLog(@"Image Received ");
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            self.imageFeedView.image = image;
+            
+        });
+    }];
+    
+    [task resume];
+}
+
+
 
 @end
 
