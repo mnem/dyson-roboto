76b02d4a810fd4338b047f9a0a706965f0922153
David Wagner <mnem@noiseandheat.com>
2016-09-08 14:02:32 +0100

    Skeleton app

---
+++ Ultratron/Ultratron/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ultratron
+//
+//  Created by David Wagner on 08/09/2016.
+//  Copyright © 2016 Noise & Heat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ultratron/Ultratron/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ultratron
+//
+//  Created by David Wagner on 08/09/2016.
+//  Copyright © 2016 Noise & Heat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
